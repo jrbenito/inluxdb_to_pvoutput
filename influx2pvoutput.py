67ccf8c860730427dacf72dd41b3e49977ef2aae
Josenivaldo Benito Jr <jbenito@rxnetworks.com>
2021-05-10 08:14:08 -0700

    Add test-script and pvoutput class
    - test script print a table with current day every 5 minutes
    
    Signed-off-by: Josenivaldo Benito Jr <jbenito@rxnetworks.com>

--- influx2pvoutput.py
+++ influx2pvoutput.py
@@ -17,6 +17,108 @@ INFLUX_URL = config['INFLUX_URL']
 
 LOCAL_TZ = timezone(config['TIME_ZONE'])
 
+class PVOutputAPI(object):
+
+    def __init__(self, API, system_id=None):
+        self._API = API
+        self._systemID = system_id
+        self._wh_today_last = 0
+
+    def add_status(self, payload, system_id=None):
+        """Add live output data. Data should contain the parameters as described
+        here: http://pvoutput.org/help.html#api-addstatus ."""
+        sys_id = system_id if system_id is not None else self._systemID
+        self.__call("https://pvoutput.org/service/r2/addstatus.jsp", payload, sys_id)
+
+    def add_output(self, payload, system_id=None):
+        """Add end of day output information. Data should be a dictionary with
+        parameters as described here: http://pvoutput.org/help.html#api-addoutput ."""
+        sys_id = system_id if system_id is not None else self._systemID
+        self.__call("http://pvoutput.org/service/r2/addoutput.jsp", payload, sys_id)
+
+    def __call(self, url, payload, system_id=None):
+        headers = {
+            'X-Pvoutput-Apikey': self._API,
+            'X-Pvoutput-SystemId': system_id,
+            'X-Rate-Limit': '1'
+        }
+
+        # Make tree attempts
+        for i in range(3):
+            try:
+                r = requests.post(url, headers=headers, data=payload, timeout=10)
+                reset = round(float(r.headers['X-Rate-Limit-Reset']) - time())
+                if int(r.headers['X-Rate-Limit-Remaining']) < 10:
+                    print("Only {} requests left, reset after {} seconds".format(
+                        r.headers['X-Rate-Limit-Remaining'],
+                        reset))
+                if r.status_code == 403:
+                    print("Forbidden: " + r.reason)
+                    sleep(reset + 1)
+                else:
+                    r.raise_for_status()
+                    break
+            except requests.exceptions.HTTPError as errh:
+                print(localnow().strftime('%Y-%m-%d %H:%M'), " Http Error:", errh)
+            except requests.exceptions.ConnectionError as errc:
+                print(localnow().strftime('%Y-%m-%d %H:%M'), "Error Connecting:", errc)
+            except requests.exceptions.Timeout as errt:
+                print(localnow().strftime('%Y-%m-%d %H:%M'), "Timeout Error:", errt)
+            except requests.exceptions.RequestException as err:
+                print(localnow().strftime('%Y-%m-%d %H:%M'), "OOps: Something Else", err)
+
+            sleep(5)
+        else:
+            print(localnow().strftime('%Y-%m-%d %H:%M'),
+                  "Failed to call PVOutput API after {} attempts.".format(i))
+
+    def send_status(self, date, energy_gen=None, power_gen=None, energy_imp=None,
+                    power_imp=None, temp=None, vdc=None, cumulative=False, vac=None,
+                    temp_inv=None, energy_life=None, comments=None, power_vdc=None,
+                    system_id=None):
+        # format status payload
+        payload = {
+            'd': date.strftime('%Y%m%d'),
+            't': date.strftime('%H:%M'),
+        }
+
+        # Only report total energy if it has changed since last upload
+        # this trick avoids avg power to zero with inverter that reports
+        # generation in 100 watts increments (Growatt and Canadian solar)
+        if ((energy_gen is not None) and (self._wh_today_last != energy_gen)):
+            self._wh_today_last = int(energy_gen)
+            payload['v1'] = int(energy_gen)
+
+        if power_gen is not None:
+            payload['v2'] = float(power_gen)
+        if energy_imp is not None:
+            payload['v3'] = int(energy_imp)
+        if power_imp is not None:
+            payload['v4'] = float(power_imp)
+        if temp is not None:
+            payload['v5'] = float(temp)
+        if vdc is not None:
+            payload['v6'] = float(vdc)
+        if cumulative is True:
+            payload['c1'] = 1
+        else:
+            payload['c1'] = 0
+        if vac is not None:
+            payload['v8'] = float(vac)
+        if temp_inv is not None:
+            payload['v9'] = float(temp_inv)
+        if energy_life is not None:
+            payload['v10'] = int(energy_life)
+        if comments is not None:
+            payload['m1'] = str(comments)[:30]
+        # calculate efficiency
+        if ((power_vdc is not None) and (power_vdc > 0) and (power_gen is not None)):
+            payload['v12'] = float(power_gen) / float(power_vdc)
+
+        # Send status
+        self.add_status(payload, system_id)
+
+
 # Local time with timezone
 def localnow():
     return datetime.now(LOCAL_TZ)
@@ -24,7 +126,7 @@ def localnow():
 # Aplication loop
 def main_loop():
     # init
-    #pvo = PVOutputAPI(APIKEY, SYSTEMID)
+    pvo = PVOutputAPI(APIKEY, SYSTEMID)
 
     # Loop until end of universe
     while True:
---
+++ test-script/summarize-today.py
@@ -0,0 +1,64 @@
+import influxdb_client
+from datetime import datetime, timedelta
+from pytz import timezone
+from configobj import ConfigObj
+
+config = ConfigObj("pvoutput.conf")
+bucket = config['INFLUX_BUCKET']
+org = config['INFLUX_ORG']
+token = config['INFLUX_TOKEN']
+# Store the URL of your InfluxDB instance
+url=config['INFLUX_URL']
+
+client = influxdb_client.InfluxDBClient(
+    url=url,
+    token=token,
+    org=org
+)
+
+query_api = client.query_api()
+
+localTz = timezone('america/Sao_Paulo')
+utcTz = timezone('UTC')
+
+# Locatime
+localNow = datetime.now(localTz)
+
+startTime = localNow.replace(hour=0, minute=0, second=0, microsecond=0).astimezone(utcTz)
+endTime = localNow.replace(hour=23, minute=59, second=59, microsecond=9999).astimezone(utcTz)
+
+p = {
+    "_start" : startTime,
+    "_end" : endTime,
+    "_every": timedelta(minutes=5)
+}
+
+query = '''m_pwr_vdc = from(bucket: "power_plant")\
+|> range(start: _start, stop: _end)\
+|> filter(fn: (r) => r["name"] == "Inverter" and r["_measurement"] == "modbus" and r["type"] == "input_register") \
+|> filter(fn: (r) => r["_field"] == "power_output" or r["_field"] == "Vdc1" or r["_field"] == "Vac" or r["_field"] == "temperature") \
+|> aggregateWindow(every: _every, fn: mean, createEmpty: false) \
+|> pivot(columnKey:["_field"], rowKey:["_time"], valueColumn:"_value") \
+etoday = from(bucket: "power_plant") \
+|> range(start: _start, stop: _end)\
+|> filter(fn: (r) => r["name"] == "Inverter" and r["_measurement"] == "modbus" and r["type"] == "input_register") \
+|> filter(fn: (r) => r["_field"] == "energy_today" or r["_field"] == "energy_total") \
+|> aggregateWindow(every: _every, fn: max, createEmpty: false) \
+|> pivot(columnKey:["_field"], rowKey:["_time"], valueColumn:"_value") \
+join(tables: {etoday: etoday, pwr_vdc: m_pwr_vdc}, on: ["_time", "name", "_measurement", "type", "_start",  "_stop"]) \
+|> sort(columns: ["_time"]) \
+'''
+
+result = client.query_api().query(query=query, params=p)
+
+results = []
+for table in result:
+    for record in table.records:
+        print(record["_time"].astimezone(localTz).strftime('%H:%M') + ',' +
+              str(record["power_output"]) + ',' +
+              str(record["energy_today"]) + ',' +
+              str(record["Vac"]) + ',' +
+              str(record["Vdc1"]) + ',' +
+              str(record["energy_total"]) + ',' +
+              str(record["temperature"])
+              )
